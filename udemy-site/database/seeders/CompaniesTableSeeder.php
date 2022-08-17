38efa440e8eddec62d831238b44d480c075cdc0d
taredseb9 <taredseb9@gmail.com>
2022-08-17 16:27:52 +0300

    Udemy introduction finished

---
+++ udemy-site/database/seeders/CompaniesTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class CompaniesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        // DB::table('companies')->truncate();
+
+        $companies = [];
+
+        $faker = Faker::create();
+
+        foreach(range(1,10) as $index){
+            $companies[] = [
+                'name' => $faker->company(),
+                'address' => $faker->address(),
+                'website' => $faker->domainName(),
+                'email' => $faker->email(),
+                'created_at' => now(),
+                'updated_at' => now(),
+                // 'name' => $name = "Company $index",
+                // 'address' => "Address $name",
+                // 'website' => "Website $name",
+                // 'email' => "Email $name",
+                // 'created_at' => now(),
+                // 'updated_at' => now(),
+
+            ];
+        }
+
+        DB::table('companies')->insert($companies);
+
+    }
+}
---
+++ udemy-site/database/seeders/ContactsTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('contacts')->truncate();
+
+        $contacts = [];
+
+        $faker = Faker::create();
+
+        foreach(range(1,10) as $index){
+            $contacts[] = [
+                'first_name' => $faker->firstName(),
+                'last_name' => $faker->lastName(),
+                'phone' => $faker->phoneNumber(),
+                'email' => $faker->email(),
+                'address' => $faker->address(),
+                'company_id' => $faker->company(),
+                'created_at' => now(),
+                'updated_at' => now(),
+                // 'name' => $name = "Company $index",
+                // 'address' => "Address $name",
+                // 'website' => "Website $name",
+                // 'email' => "Email $name",
+                // 'created_at' => now(),
+                // 'updated_at' => now(),
+
+            ];
+        }
+
+        DB::table('contacts')->insert($contacts);
+    }
+}
