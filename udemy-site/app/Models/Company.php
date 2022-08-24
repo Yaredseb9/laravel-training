5058175a53fec6c153a449266ad17ba50a27e02c
taredseb9 <taredseb9@gmail.com>
2022-08-24 08:54:18 +0300

    updates

--- udemy-site/app/Models/Company.php
+++ udemy-site/app/Models/Company.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    public function contacts(){
+        return $this->hasMany(Contact::class);
+    }
 }
