da528b04c0d950aadb046ddc77f1d1ccb5304377
taredseb9 <taredseb9@gmail.com>
2022-08-31 17:51:07 +0300

    completed 05 - Rendering Dynamic Data

--- udemy-site/app/Http/Controllers/ContactController.php
+++ udemy-site/app/Http/Controllers/ContactController.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function index(){
-        return view('contacts.index');
+    public function index()
+    {
+        // $companies = Company::select('id','name')->prepend('','All Company')->get();
+        $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Company', '');
+        $contacts = Contact::orderBy('first_name', 'asc')->where(function ($query) {
+            if ($compony_id = request('company_id')) {
+                $query->where('company_id', $compony_id);
+            }
+        })->paginate(10);
+        return view('contacts.index', compact('contacts', 'companies'));
     }
-    
-    public function create(){
+
+    public function create()
+    {
         return view('contacts.create');
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $contact =  Contact::find($id);
         return view('contacts.show', compact('contact'));
     }
