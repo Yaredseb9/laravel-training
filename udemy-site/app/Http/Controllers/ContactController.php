5a764539c34a07044270acd65f22d64c6bc5674e
yaredseb9 <yaredseb9@gmail.com>
2022-09-04 17:12:57 +0300

    update on 9-4-2022

--- udemy-site/app/Http/Controllers/ContactController.php
+++ udemy-site/app/Http/Controllers/ContactController.php
@@ -16,7 +16,11 @@ class ContactController extends Controller
             if ($compony_id = request('company_id')) {
                 $query->where('company_id', $compony_id);
             }
+            if ($search = request('search')) {
+                $query->where('first_name', 'LIKE', "%{$search}%");
+            }
         })->paginate(10);
+
 
         // dd($contacts);
         return view('contacts.index', compact('contacts', 'companies'));
@@ -38,9 +42,8 @@ class ContactController extends Controller
             'company_id' => 'required|exists:companies,id'
         ]);
 
-        // dd($request->except('_token','_method'));
 
-        Contact::create($request->except('_token','_method'));
+        Contact::create($request->except('_token', '_method'));
 
         // $contact = new Contact();
         // $contact->first_name = $request->first_name;
@@ -52,25 +55,24 @@ class ContactController extends Controller
         // $contact->save();
 
         return redirect()->route('contacts.index')->with('message', 'Contact has been added successfully.');
-
     }
     public function show($id)
     {
         $contact =  Contact::findOrFail($id);
         return view('contacts.show', compact('contact'));
     }
-    
+
     public function edit($id)
     {
         $contact =  Contact::findOrFail($id);
         $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Company', '');
-        
-        return view('contacts.edit', compact('contact','companies'));
+
+        return view('contacts.edit', compact('contact', 'companies'));
     }
-    
-    public function update( $id, Request $request)
+
+    public function update($id, Request $request)
     {
-        
+
         $request->validate([
             'first_name' => 'required',
             'last_name' => 'required',
@@ -80,7 +82,7 @@ class ContactController extends Controller
         ]);
 
         $contact = Contact::findOrFail($id);
-        $contact->update($request->except('_token','_method'));
+        $contact->update($request->except('_token', '_method'));
 
         return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
     }
