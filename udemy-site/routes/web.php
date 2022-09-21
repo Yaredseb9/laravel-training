98fe0284667fefc9a5180fe0fbaf3af53c6c863b
yaredseb9 <yaredseb9@gmail.com>
2022-09-21 13:03:06 +0300

    Completed 005 Resource Routes

--- udemy-site/routes/web.php
+++ udemy-site/routes/web.php
@@ -24,14 +24,21 @@ Route::get('/', function () {
 //     return view('contacts.index');
 // })->name('contacts.index');
 Route::middleware('auth')->group(function(){
-    Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
-    Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
-    Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
-    // Route::get('/contacts/{contact:first_name}', [ContactController::class, 'show'])->name('contacts.show');
-    Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
-    Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
-    Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
-    Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+    // Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
+    // Route::post('/contacts', [ContactController::class, 'store'])->name('contacts.store');
+    // Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+    // // Route::get('/contacts/{contact:first_name}', [ContactController::class, 'show'])->name('contacts.show');
+    // Route::get('/contacts/{contact}', [ContactController::class, 'show'])->name('contacts.show');
+    // Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
+    // Route::put('/contacts/{contact}', [ContactController::class, 'update'])->name('contacts.update');
+    // Route::delete('/contacts/{contact}', [ContactController::class, 'destroy'])->name('contacts.destroy');
+    // Route::resource('/contacts', ContactController::class);
+
+    // Route::resource('/contacts', ContactController::class);
+    Route::resources([
+        '/contacts' => ContactController::class,
+        '/companies' => CompanyController::class
+    ]);
 });
 // Route::get('/contacts/create',function(){
 //     return view('contacts.create');
