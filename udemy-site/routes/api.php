382bf5ee98dbdcad891aea126c8f2a9bdb6e1ac0
yaredseb9 <yaredseb9@gmail.com>
2022-09-21 14:44:49 +0300

    Completed 007 API Resourceful Route

--- udemy-site/routes/api.php
+++ udemy-site/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::apiResource('/contacts', ContactController::class);
+Route::apiResources([
+    '/contacts' => ContactController::class,
+    '/companies' => CompanyController::class
+]);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- udemy-site/routes/web.php
+++ udemy-site/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CompanyController;
 use App\Models\Contact;
 use Illuminate\Support\Facades\Route;
 
