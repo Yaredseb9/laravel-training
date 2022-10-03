bd2a7ccba102acef4ba07b94578d9ce7eed5dcbb
yaredseb9 <yaredseb9@gmail.com>
2022-10-03 16:50:39 +0300

    Completed 004 Advanced Eager Loading

--- udemy-site/app/Models/User.php
+++ udemy-site/app/Models/User.php
@@ -33,6 +33,8 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+    // protected $whith = ['contacts','companies'];
+
     /**
      * The attributes that should be cast.
      *
